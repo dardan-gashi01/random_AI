dd49a111f111cc5cf6a82ae92f0f2bb53bbad404
dardan-gashi01 <dardan-gashi01@outlook.com>
2021-12-05 21:08:44 +0000

    starting to implement dijkstra into task1
    wrapping my head around the looking at neighbours that are in bound and not out of bound

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #setting the size of the Grid
-Gridsize = 6
+Gridsize = 3
 
 #creating a grid of size Gridsize and generating all of the grids with random valuesf rom 1-9
 grid = np.random.randint(0, 9, size=(Gridsize, Gridsize))
@@ -36,51 +36,86 @@ def shortestPath1(graph, start, end):
     current = start
     x,y = 0,0
     path = []
-    path.append(current)
+    path.append((x,y))
     print(current)
     
-    #if ((x>0 or y>0) and x+1< len(graph) and y+1<len(graph)):
+    unvisited = np.zeros((Gridsize,Gridsize),dtype=bool)
+    dist=np.ones((Gridsize,Gridsize),dtype=int)*np.Infinity
+    #making the starting point 0
+    dist[0,0] = 0
+    visited = []
+    print(unvisited)
     
-    while x != Gridsize-1 or y != Gridsize-1:
-        if x+1 < graph.shape[0]:
-            if y+1 < graph.shape[1]:
-                if (graph[y,x+1] > graph[y+1,x]):
-                    print(grid[y+1, x])
-                    print("down")
-                    current = grid[y+1,x]
-                    y+=1
-                    print(y)
-                    path.append(current)
-                    print(path)
-                else:
-                    print(grid[y,x+1])
-                    print("right")
-                    current = grid[y,x+1]
-                    x+=1
-                    print(x)
-                    path.append(current)
-                    print(path)
-            else:
-                print(grid[y,x+1])
-                print("right")
-                current = grid[y,x+1]
-                x+=1
-                print(x)
-                path.append(current)
-                print(path)
-        else:
-            print(grid[y+1,x])
-            print("down")
-            current = grid[y+1,x]
-            y+=1
-            print(y)
-            path.append(current)
-            print(path)
-            
-            
+    #print(visited)
+    complete = False
+    
+    
+    # while not complete:
+    #to look to the right if we have any neighbours there
+    if x < Gridsize -1:
+        print(x+1, y)
+    #to look to the left if we have any neighbours there
+    if x > 0:
+        print(x-1,y)
+    #to look down to see if we have any neighbours there
+    if y < Gridsize -1:
+        print(x,y+1)
+    if y > 0:
+        print(x,y-1)        
         
-
-
     
-     
+    #while (len(visited) < GridSize):
+        
+    #while not complete:
+        
+    #if ((x>0 or y>0) and x+1< len(graph) and y+1<len(graph)):
+        
+        
+    
+    #
+    #brute force answer
+    #
+    # while x != Gridsize-1 or y != Gridsize-1:
+    #     #looking right to see if its in bounds if it goes right
+    #     if x+1 < graph.shape[0]:
+    #         #looking right to see if its in bounds if it goes down
+    #         if y+1 < graph.shape[1]:
+    #             if (graph[y,x+1] > graph[y+1,x]):
+    #                 print(grid[y+1, x])
+    #                 print("down")
+    #                 current = grid[y+1,x]
+    #                 y+=1
+    #                 print(y)
+    #                 path.append((x,y))
+    #                 print(path)
+    #             else:
+    #                 print(grid[y,x+1])
+    #                 print("right")
+    #                 current = grid[y,x+1]
+    #                 x+=1
+    #                 print(x)
+    #                 path.append((x,y))
+    #                 print(path)
+    #         else:
+    #             print(grid[y,x+1])
+    #             print("right")
+    #             current = grid[y,x+1]
+    #             x+=1
+    #             print(x)
+    #             path.append((x,y))
+    #             print(path)
+    #     else:
+    #         print(grid[y+1,x])
+    #         print("down")
+    #         current = grid[y+1,x]
+    #         y+=1
+    #         print(y)
+    #         path.append((x,y))
+    #         print(path)
+            
+            
+    #highlight the path that it is moving
+    
 shortestPath1(grid, grid[0,0], grid[Gridsize-1,Gridsize-1])
+
+# def shortestPath2():
